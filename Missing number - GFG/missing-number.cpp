633a8e5e33250cc8ae93d0ba64422ab8c25ddc73
priyanshubisht2002 <120446275+priyanshubisht2002@users.noreply.github.com>
2023-05-17 14:27:11 +0530

    Added solution - LeetHub

--- Missing number - GFG/missing-number.cpp
+++ Missing number - GFG/missing-number.cpp
@@ -26,12 +26,11 @@ int main()
 int missingNumber(int A[], int N)
 {
     int xor1=0,xor2=0;
-    for(int i=1;i<=N;i++){
-        xor1=xor1^i;
-    }
     for(int i=0;i<N-1;i++){
         xor2=xor2^A[i];
+      xor1= xor1^(i+1);
     }
+    xor1= xor1^N;
     int ans =xor1^xor2;
     return ans;
 }
