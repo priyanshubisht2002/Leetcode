cf7b3d288291c79111726e86ce0bb0e48b88ed89
priyanshubisht2002 <120446275+priyanshubisht2002@users.noreply.github.com>
2023-05-17 11:42:38 +0530

    Added solution - LeetHub

---
+++ Missing number - GFG/missing-number.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int missingNumber(int a[], int n);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int i=0, n;
+		cin>>n;
+		int a[n+5];
+		for(i=0;i<n-1;i++)
+			cin>>a[i];
+			
+		cout<<missingNumber(a, n)<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+int missingNumber(int A[], int N)
+{
+    int xor1=0,xor2=0;
+    for(int i=1;i<=N;i++){
+        xor1=xor1^i;
+    }
+    for(int i=0;i<N-1;i++){
+        xor2=xor2^A[i];
+    }
+    int ans =xor1^xor2;
+    return ans;
+}
